cb1a945e98045fedaaab13eff050f62d8660e8d3
Pierre Mourret <pierre.mourret@protonmail.com>
2023-05-27 22:41:52 +0200

    ajout sidebar

--- app/core_module/settings.py
+++ app/core_module/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'core_module.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files (upload from users)
